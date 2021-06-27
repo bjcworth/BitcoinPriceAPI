ccf74794022634a3c4710258f297e70adbf0ac43
Brandon Charlesworth <bjcworth92@gmail.com>
2021-06-27 16:11:51 -0400

    Added Newtonsoft.json dependency package and used it to swap out hard-coded price data with live CoinDesk API data.

--- BitcoinPriceAPI/Controllers/BitcoinPriceIndexController.cs
+++ BitcoinPriceAPI/Controllers/BitcoinPriceIndexController.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net;
 
 namespace BitcoinPriceAPI.Controllers
 {
@@ -23,19 +27,23 @@ namespace BitcoinPriceAPI.Controllers
         }
         static BitcoinPriceIndexController()
         {
-            _prices = new BitcoinPriceIndex
-            {
-                USD = "1,044.3345",
-                GBP = "22,358.1918",
-                EUR = "26,568.8183",
-                LastUpdated = "Jun 27, 2021 16:23:00 UTC"
-            };
+            _prices = new BitcoinPriceIndex { };
 
         }
 
         [HttpGet]
         public BitcoinPriceIndex GetPrices()
         {
+            WebClient Client = new WebClient();
+            string PricesString = Client.DownloadString("https://api.coindesk.com/v1/bpi/currentprice.json");
+            JObject PricesObject = JObject.Parse(PricesString);
+
+            _prices.USD = PricesObject["bpi"]["USD"]["rate"].ToString();
+            _prices.GBP = PricesObject["bpi"]["GBP"]["rate"].ToString();
+            _prices.EUR = PricesObject["bpi"]["EUR"]["rate"].ToString();
+            _prices.LastUpdated = PricesObject["time"]["updated"].ToString();
+
+
             return (_prices);
         }
     }
